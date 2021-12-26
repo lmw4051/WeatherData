d7e086afd5180b6e7cacc96867aed642d3693f7e
David Lee <david.m.lee@ihwic.com>
2021-12-26 19:58:10 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by David on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
