de100da6ca55b445031730c09bab58e357c637ea
David Lee <david.m.lee@ihwic.com>
2021-12-26 21:52:47 +0800

    Adding LocationManager

---
+++ Weather/Managers/LocationManager.swift
@@ -0,0 +1,38 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by David on 2021/12/26.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+  let manager = CLLocationManager()
+  
+  @Published var location: CLLocationCoordinate2D?
+  @Published var isLoading = false
+  
+  override init() {
+    super.init()
+    manager.delegate = self
+  }
+  
+  func requestLocation() {
+    isLoading = true
+    manager.requestLocation()
+  }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    location = locations.first?.coordinate
+    isLoading = false
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    print("Error getting location", error)
+    isLoading = false
+  }
+}
---
+++ Weather/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by David on 2021/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {  
+  var body: some View {
+    Text("Hello World")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
