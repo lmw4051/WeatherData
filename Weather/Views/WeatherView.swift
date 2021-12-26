c722efde19c3bcdd705ce22d8efe838f332754af
David Lee <david.m.lee@ihwic.com>
2021-12-26 23:02:00 +0800

    Adding Name and Date UI elements

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -11,7 +11,26 @@ struct WeatherView: View {
   var weather: ResponseBody
   
   var body: some View {
-    Text(weather.name)
+    ZStack(alignment: .leading) {
+      VStack {
+        VStack(alignment: .leading, spacing: 5) {
+          Text(weather.name)
+            .bold()
+            .font(.title)
+          
+          Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+            .fontWeight(.light)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Spacer()
+      }
+      .padding()
+      .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    .edgesIgnoringSafeArea(.bottom)
+    .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+    .preferredColorScheme(.dark)
   }
 }
 
